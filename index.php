c43fc5ab714158b8da76084e03475203e640f11f
Eric Gregoire <09egrego@gmail.com>
2014-07-13 01:10:34 -0500

    Added padding and margin classes

---
+++ index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Zolatone | Welcome</title>
+        <link rel="stylesheet" href="css/foundation.css" />
+    </head>
+    <body>
+        <?
+
+        ?>
+        <p> <? echo "php stuff in short tags" ?> </p>
+        <a class="button" href="">How Dis?</a>
+        <div class="background-primary small-padding-3">
+            <a class="button secondary margin-0" href="">How Dis?</a>
+        </div>
+        <form class="row">
+            <fieldset>
+                <legend>Customer Info:</legend>
+                <div>
+                    <span class="error">This is an error</span>
+                    <label>Name:<input type="email"  /></label>
+                </div>
+            </fieldset>
+        </form>
+
+        <script src="js/vendor/jquery.js"></script>
+        <script src="js/foundation.min.js"></script>
+        <script>
+            $(document).foundation();
+        </script>
+    </body>
+</html>
