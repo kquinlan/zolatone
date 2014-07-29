7526687af14c449f19adfb241c0715f5729dea9a
Eric Gregoire <09egrego@gmail.com>
2014-07-28 22:50:01 -0500

    Finalized Off-canvas nav
    Finalized foundation for the off-canvas mobile navigation.

--- common/header.php
+++ common/header.php
@@ -1,4 +1,48 @@
 <!-- header.php -->
 <header>
-	
+<!-- Main Navigation -->
+
+<!-- Mobile Navigation -->
+<nav class="mobile">
+	<div class="off-canvas-wrap" data-offcanvas>
+		<div class="inner-wrap">
+
+		<!-- Off Canvas Button -->
+		<section class="right toggle">
+			<a class="right-off-canvas-toggle small-margin-1" href="" ></a>
+		</section>
+
+		<!-- Off Canvas Menu -->
+		<aside class="right-off-canvas-menu">
+			<ul class="off-canvas-list">
+				<li><a href="#">Home</a></li>
+				<li><a href="#">About</a></li>
+				<li><a href="#">Finishes</a></li>
+				<li><a href="#">Ideas</a></li>
+				<li><a href="#">Tech Info</a>
+					<ul>
+						<a href="#">Tech Docs</a>
+						<a href="#">Videos</a>
+						<a href="#">LEED</a>
+					</ul>
+				</li>	
+				<li><a href="#">Sample Room</a>
+					<ul>
+						<a href="#">Login / Sign Up</a>
+						<a href="#">My Saved Samples</a>
+					</ul>
+				</li>
+				<li><a href="#">Subscribe</a></li>
+				<li><a href="#">Where To Buy</a></li>
+				<li><a href="#">FAQ</a></li>
+				<li><a href="#">Contact</a></li>
+			</ul>
+		</aside>
+
+		<a class="exit-off-canvas"></a>
+
+		</div>
+	</div>
+</nav>
+
 </header>
