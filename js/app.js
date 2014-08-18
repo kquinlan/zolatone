a8bfeee96f11c871f4798dd76ca529e9652de47c
Eric Gregoire <eric.gregoire@renaissance.com>
2014-08-18 09:58:59 -0500

    Moved js configs to app.js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,29 @@
-$(window).scroll(function(){
-    if ($(this).scrollTop() > 195) {
-        $('.menu-mobile').addClass('fixed');
-    } else {
-        $('.menu-mobile').removeClass('fixed');
-    }
+// Optional params for foundation
+$(document).foundation();
+
+// Enhance responsiveness on mobile
+$(function() {
+    FastClick.attach(document.body);
+});
+
+// Slider parameters
+$(".rslides").responsiveSlides({
+    auto: true,             // Boolean: Animate automatically, true or false
+    speed: 1500,            // Integer: Speed of the transition, in milliseconds
+    timeout: 6000,          // Integer: Time between slide transitions, in milliseconds
+    pager: false,           // Boolean: Show pager, true or false
+    nav: false,             // Boolean: Show navigation, true or false
+    random: false,          // Boolean: Randomize the order of the slides, true or false
+    pause: false,           // Boolean: Pause on hover, true or false
+    pauseControls: true,    // Boolean: Pause when hovering controls, true or false
+    prevText: "Previous",   // String: Text for the "previous" button
+    nextText: "Next",       // String: Text for the "next" button
+    maxwidth: "",           // Integer: Max-width of the slideshow, in pixels
+    navContainer: "",       // Selector: Where controls should be appended to, default is after the 'ul'
+    manualControls: "",     // Selector: Declare custom pager navigation
+    namespace: "rslides",   // String: Change the default namespace used
+    before: function(){},   // Function: Before callback
+    after: function(){}     // Function: After callback
 });
 
 // Navigation panel display events
