0cdb8bd18197ef2edfa21b6731314a46830a954e
Eric Gregoire <eric.gregoire@renaissance.com>
2014-08-18 10:21:19 -0500

    smaller back to top link

--- common/footer.php
+++ common/footer.php
@@ -110,7 +110,7 @@
 		<!-- Back to Top Link -->
 		<div class="row small-padding-top-1 small-padding-bottom-1">
 			<div class="medium-6 columns">
-				<a href="" class="color-white">BACK TO TOP</a>
+				<a href="" class="color-white text-smaller">BACK TO TOP</a>
 			</div>
 		</div>
 
