be29369a11cae1bb9d46e57366a2b886edbbadf8
Eric Gregoire <09egrego@gmail.com>
2014-08-22 21:45:12 -0500

    New headers added

--- common/slider.php
+++ common/slider.php
@@ -1,6 +1,8 @@
 <ul class="slider">
     <li class="lluminations"></li>
     <li class="elementary"></li>
+    <li class="alamo-plaza"></li>
+    <li class="columns-counterpointe"></li>
 </ul>
 
 <div class="to-content background-lightened small-padding-1">
