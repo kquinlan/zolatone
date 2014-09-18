ec8c23962c5b253380b9db0bfc2bc1a6a4a97d0a
Eric Gregoire <09egrego@gmail.com>
2014-09-17 23:13:01 -0500

    bold links

--- sample-room/userSamples.php
+++ sample-room/userSamples.php
@@ -1,7 +1,7 @@
 <div class="small-12 columns small-margin-bottom-2">
     <div class="color-thumb small-6 medium-4 large-4 columns left" ng-repeat="color in userColors">
         <div ng-style="{'background-image':'url(/img/samples/thumbs/' + color.name + '.jpg)'}"></div>
-        <p class="color-primary text-smaller">{{ color.name }} <a class="right" ng-click="deleteSample(color.id)">Remove</a></p>
+        <p class="color-primary text-smaller"><b>{{ color.name }}</b><a class="right" ng-click="deleteSample(color.id)"><b>Remove</b></a></p>
         <p class="color-primary text-smaller right small-margin-0"></p>
     </div>
     <p class="text-center small-margin-0" ng-show="userColors.length === 0">You have no saved samples yet.</p>
