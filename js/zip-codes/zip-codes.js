3d26bdeded9cad12e441093258761225399e5389
Eric Gregoire <09egrego@gmail.com>
2015-01-05 21:22:06 -0600

    Added zip code territories

--- js/zip-codes/zip-codes.js
+++ js/zip-codes/zip-codes.js
@@ -1,12 +1,8 @@
 angular.module('zipCodes', [])
 
 .controller('zipCodesCtrl', function($scope, $http) {
-
 	$http({method: 'GET', url: '/js/zip-codes/distributors.json'}).
     success(function(data, status, headers, config) {
-
 		$scope.distributors = data;
-
     })
-
 });
